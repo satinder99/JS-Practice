f38f62b44af49d755c3d59c028e06daa77bb2113
Satinder Singh <kainth00013@gmail.com>
2023-10-05 07:48:18 -0400

    variables and datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID=144556;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Ludhinana"
+
+
+let accountState;
+//accountID = 2
+
+accountEmail = "satinder@google.com"
+accountPassword = "33332222"
+accountCity = "Delhi"
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+ 
+/**
+ * Prefer not to use VAR, 
+ * 
+ * because of issue in block scope and functional scope
+ * 
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3) //we are sing node js not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("Satinder")
+
+let name = "Satinder" //String
+let age = 23 //Number
+let isLoggedIn = false  //boolean
+//=====================premitive============
+//number => 2^53
+//big int
+
+//String => "" or ''
+
+//boolean => true or false
+
+//null => Standalone value also a datatype
+//            represent a empty h(khali h)
+
+//undefine => when a variable i defined but not having any value yet(different from null)
+
+//symbol => unique
+//==========================================
+
+//=====================Object type=========
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
+
