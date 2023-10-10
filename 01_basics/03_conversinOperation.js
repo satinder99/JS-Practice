9c3befe0c6331af10328ad5c4f61f44adcd4d875
Satinder Singh <kainth00013@gmail.com>
2023-10-10 02:43:09 -0400

    Learned the comparision operator in JS

--- 01_basics/03_conversinOperation.js
+++ 01_basics/03_conversinOperation.js
@@ -77,7 +77,55 @@ function convToStr(num){
     console.table([str, typeof str]);
 }
 
-convToStr(someNum1);
-convToStr(someNum2);
-convToStr(someNum3);
-convToStr(someNum4);
+// convToStr(someNum1);
+// convToStr(someNum2);
+// convToStr(someNum3);
+// convToStr(someNum4);
+
+
+//************** OPERATIONS *********** */
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**2); //4
+// console.log(2/0); //infinity
+// console.log(2%3); //2
+
+let str1 = "hello"
+let str2 = "satinder"
+
+let str3 = str1 + str2
+
+//console.log(str3) //hellosatinder
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+// console.log(+true); //1
+// console.log(-true); //-1
+// console.log(+false); //0
+// console.log(-false); //-0
+
+// console.log(+""); //0
+// console.log(-""); //-0
+
+// console.log(null); //null
+// console.log(+null); //0
+// console.log(-null); //-0
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2 //not recomended
+
+let gameCounter = 100
+gameCounter++ //postfix
+console.log(gameCounter); //101
+++gameCounter //prefix
+console.log(gameCounter);//102
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// > < <= >= == != 
+
+// console.log("2">1); //TRUE
+// console.log("02">1); //TRUE
+
+
+console.log( null > 0); //FALSE
+console.log( null == 0); //false
+console.log( null >= 0); //true
+
+/**
+ * The reason is that an equality check == comparision ><>=<= works differently
+ * 
+ * Comparision convert null to a number , treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false
+ */
+
+
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false
+console.log(undefined > 0); //false
