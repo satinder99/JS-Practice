661ec9bab1c05bfb1286354730dbefe747d19ffc
Satinder Singh <kainth00013@gmail.com>
2023-10-20 23:21:18 -0400

    BMI index created

---
+++ Project01/BMI-Index/scripts/bmicalculator.js
@@ -0,0 +1,15 @@
+function calc(){
+    let height = document.querySelector('#height').value;
+    let weight =  document.querySelector('#weight').value;
+
+    let myBMI = Math.round(weight/(height*height));
+    console.log(myBMI);
+    let result=document.querySelector('#result');
+    result.textContent = `BMI INDEX : ${myBMI}`
+    if(myBMI<18.5 || myBMI>22){
+        result.style.border = "2px dotted red";
+    }
+    else{
+        result.style.border = "2px dotted green";
+    }
+}
